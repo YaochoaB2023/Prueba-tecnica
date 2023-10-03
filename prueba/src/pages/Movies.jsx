822143a560880fe7ad2c02f537d78dc870c0b38d
yaochoaB2023 <ochoabautistayeisonalberto@gmail.com>
2023-10-03 11:23:15 -0500

    creacion de la home page series y movies

---
+++ prueba/src/pages/Movies.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavBar } from '../components/NavBar'
+import { Footer } from '../components/Footer'
+
+export const Movies = () => {
+  return (
+    <>
+      <NavBar/>
+        Movies
+      <Footer/>
+    </>
+  )
+}
---
+++ prueba/src/pages/Series.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavBar } from '../components/NavBar'
+import { Footer } from '../components/Footer'
+
+export const Series = () => {
+  return (
+    <>
+      <NavBar/>
+        <h1>series</h1>
+      <Footer/>
+    </>
+  )
+}
